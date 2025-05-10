ec57bc4c734b18c883b9ee3a3e3a02cd6192437f
paoyako24 <paolaparedes248@gmail.com>
2025-05-10 14:39:07 -0600

    Add files via upload

---
+++ PAGO.PHP/PagoEmpleado.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-eqiv=" X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilos.css">
+        <title>Pago de empleado</title>
+    </head>
+    <body>
+        <header>
+            <h1 id="centrado">PAGO DE EMPLEADO</h1>
+        </header>
+        <section>
+
+        <table>
+            <form action="pago.php" method="post">
+                <tr>
+                    <td width="200">Emplado</td>
+                    <td><input type="text" name="textEmpleado" id=""></td>
+                </tr>
+        </table>
+        </section>
+    </body>
+</html>
